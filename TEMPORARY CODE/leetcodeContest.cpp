f185a396a98357221dfc3be8c5a083314226e159
hardy2p <pushpendrasinghshekhawat500@gmail.com>
2025-01-05 01:45:07 +0530

    New commits

---
+++ TEMPORARY CODE/leetcodeContest.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string p = "abcd*efg";
+    // string s1 = "";
+    // string s2 = "";
+    // int i = 0;
+    // for (; i < p.size(); i++)
+    // {
+    //     if (p[i] == '*')
+    //         break;
+    //     s1 = s1 + p[i];
+    // }
+    // i++;
+    // for (; i < p.size(); i++)
+    // {
+    //     s2 = s2 + p[i];
+    // }
+    // cout<<s1<<endl;
+    // cout<<s2<<endl;
+    string s="car*";
+    return 0;
+}
--- Tree/bfs.cpp
+++ Tree/bfs.cpp
@@ -1,53 +1,65 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-//Defining tree
-class Node{
-    public:
+// Defining tree
+class Node
+{
+public:
     int data;
-    Node* left;
-    Node* right;
-    Node(int data){
-        this->data=data;
-        this->left=NULL;
-        this->right=NULL;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
     }
 };
 
-//Tree traversal Using BFS
- void bfsTraversal(Node* root){
-    if(root==nullptr){
+// Tree traversal Using BFS
+void bfsTraversal(Node *root)
+{
+    if (root == nullptr)
+    {
         return;
     }
 
-    queue<Node*> q;
+    queue<Node *> q;
     q.push(root);
-    while(!q.empty()){
-        Node* currNode= q.front();
-        cout<<currNode->data<<" ";
-        q.pop();
+    while (!q.empty())
+    {
+        int levelSize = q.size();
 
-        if(currNode->left != nullptr){
-            q.push(currNode->left);
-        }
+        for (int i = 0; i < levelSize; i++)
+        {
+            Node *currNode = q.front();
+            q.pop();
+            cout << currNode->data << " ";
 
-        if(currNode->right != nullptr){
-            q.push(currNode->right);
+            if (currNode->left != nullptr)
+            {
+                q.push(currNode->left);
+            }
+
+            if (currNode->right != nullptr)
+            {
+                q.push(currNode->right);
+            }
         }
     }
- }
+}
 int main()
 {
-    //Constructing a tree
-    Node* root=new Node(1);
-    Node* a=new Node(2);
-    Node* b=new Node(3);
-    root->left=a;
-    root->right=b;
-    Node* c=new Node(4);
-    Node* d=new Node(5);
-    a->left=c;
-    a->right=d;
+    // Constructing a tree
+    Node *root = new Node(1);
+    Node *a = new Node(2);
+    Node *b = new Node(3);
+    root->left = a;
+    root->right = b;
+    Node *c = new Node(4);
+    Node *d = new Node(5);
+    a->left = c;
+    a->right = d;
 
     bfsTraversal(root);
     return 0;
