3ebf76b88cbe6a70a7c095e7614e5ba2d6510ad9
hardy2p <pushpendrasinghshekhawat500@gmail.com>
2025-01-26 12:06:32 +0530

    dummy

---
+++ leetcode/bitset.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    bitset<32> bit(7);
+    cout<<bit<<endl;
+    cout<<bit[0]<<endl;//access from back
+    return 0;
+}
--- leetcode/coforge.cpp
+++ leetcode/coforge.cpp
@@ -1,8 +1,33 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World"<<endl;
+    int num1=3;
+    int num2=5;
+    
+    int n = __builtin_popcount(num2);
+    int ans = 0;
+    bitset<32> bit(num1);
+    for (int i = 31; i >= 0; i--)
+    {
+        if (bit[i] & 1 && n)
+        {
+            ans |= 1 << i;
+            n--;
+        }
+    }
+    
+    if (n > 0)
+    {
+        for (int i = 0; i < 32; i++)
+        {
+            if (!(ans & (1 << i)) && n)
+            {
+                ans |= 1 << i;
+                n--;
+            }
+        }
+    }
     return 0;
 }
